c840b5aea42099386c76a7c2ebd2dca6536bb74d
Dmitry Zubarev <DmitryZubarev@x5.ru>
2024-01-08 05:45:37 +0300

    added getMessage method

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,13 @@
 package guru.qa;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello guys!");
+        System.out.println(getMessage());
     }
+
+    private static String getMessage() {
+        return "Hello guys!";
+    }
+
 }
