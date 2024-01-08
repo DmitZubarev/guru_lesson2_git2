de0b873ad90e0808a4a447b26ef7f8f16df18d15
Dmitry Zubarev <88836901+DmitZubarev@users.noreply.github.com>
2024-01-08 05:44:34 +0300

    Update hello message

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -1,7 +1,11 @@
 package guru.qa;
 
 public class Main {
+
+    private static String msg = ""Hello guys!"";
+    
     public static void main(String[] args) {
-        System.out.println("Hello guys!");
+        System.out.println(msg);
     }
+    
 }
